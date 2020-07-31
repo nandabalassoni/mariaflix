be2bd4dace5c18db9a7b5985cee9f4cd42061d83
nandabalassoni <mariafernandabalassoni@hotmail.com>
2020-07-31 17:32:54 -0300

    criando a pagina pages dentro de components

---
+++ src/components/pages/cadastro/Video/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageDefault from '../../../PageDefault';
+
+function CadastroVideo (){
+    return (
+        <PageDefault>
+            Página de cadastro de vídeos
+        </PageDefault>
+    )
+}
+
+export default CadastroVideo;
