f1427601753ae3aaee37cf9c1e9d9d6d7a9194ea
nandabalassoni <mariafernandabalassoni@hotmail.com>
2020-07-31 17:30:11 -0300

    ajustando a pagina que pega o id 'root' no arquivo index.html

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-//import * as serviceWorker from './serviceWorker';
+import Home from './components/pages/Home';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import CadastroVideo from './components/pages/cadastro/Video'
+
+const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
