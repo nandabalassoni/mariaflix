7982c3ba36e328b967cba60091b5cf0925474e32
nandabalassoni <mariafernandabalassoni@hotmail.com>
2020-07-31 17:33:30 -0300

    criando o componente PageDefault

---
+++ src/components/PageDefault/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Menu from '../Menu';
+import Footer from '../Footer';
+
+function PageDefault ({children}) {
+    return (
+        <div>
+            <Menu />
+
+            {children}
+
+            <Footer />
+        </div>
+    );
+}
+
+export default PageDefault;
